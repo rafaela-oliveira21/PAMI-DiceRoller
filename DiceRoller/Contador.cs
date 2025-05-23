9c17fcf2fa3a688247798f5b98c2ae049415baa9
rafaela-oliveira21 <rafalelaoliveira20@email.com>
2025-05-23 20:04:37 -0300

    PAMI-DiceRoller

---
+++ DiceRoller/Contador.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class Contador
+    {
+        public int Sequencia;
+        public int Pontos;
+        public int Limite;
+        public string Venceu;
+
+        public void Jogar(Dice DadoJogador, int numeroSelecionado)
+        {
+
+            //Comparar se o selecionado é igual a facePraCima do dado do jogador
+            if (numeroSelecionado == DadoJogador.FaceParaCima)
+            {
+                //Se for igual o jogador venceu
+                Venceu = "Sim";
+                Pontos++;//Somo um ponto
+
+                if (Venceu == "Sim")
+                {
+                    Sequencia++;//Se ele tinha vencido o duelo anterior, soma 1 ponto na sequência
+                }
+            }
+            //Se não, zera a sequência
+            else
+            {   //Perdeu Playboy
+                Venceu = "Não";
+                Sequencia = 0;
+            }
+
+        }
+    }
+}
---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,56 @@
+﻿
+namespace DiceRoller
+{
+    public class Dice
+    {
+        
+        /*
+        int lados;
+        int numeracao;
+        int numeroSorteado;
+
+        public Dice(int quantidadeDeLados) { 
+            lados = quantidadeDeLados;
+        }
+        public int Rolar()
+        {
+            numeroSorteado = new Random().Next(1, lados + 1);
+            return numeroSorteado;
+        }
+        */
+
+
+        //property
+        //Atributos - utilizamos property nesse caso. (public tipo Nome)
+        public int FaceParaBaixo;
+        public int FaceParaCima;
+        public int QuantidadeDeFaces;
+
+        public Dice()
+        {
+            QuantidadeDeFaces = 6;
+        }
+        public Dice(int faces)
+        {
+            QuantidadeDeFaces = faces;
+        }
+
+        //Método responsável por sortear um número
+        public void Sortear()
+        {
+            //Gero um numero aleatório de 1 a 6
+            Random random = new Random();
+            //Jogo esse número na face para cima
+            FaceParaCima = random.Next(1,QuantidadeDeFaces + 1);
+            //FaceParaBaixo = random.Next(1, 7);  -- Método mais simples mesma usabilidade do método de cima
+            // A soma mágica = o menor lado possível + o maior
+            // A soma mágica = 1 + quantidadeDeFaces
+            FaceParaBaixo = QuantidadeDeFaces + 1 - FaceParaCima;
+            //Jogo um número na face para cima
+            //Calcular a face para baixo
+        }
+
+    }
+}
+
+    
